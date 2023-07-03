1068d9f00b15af4a463b500ab6a1c3e1dcb8c9dd
AndyWeasley2004 <m5yao@ucsd.edu>
2023-07-03 12:11:14 -0700

    Adding address, city and telephone property to Owner class. Closes #28

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -4,6 +4,9 @@ import java.util.Set;
 
 public class Owner extends Person {
     private Set<Pet> pets;
+    private String address;
+    private String city;
+    private String telephone;
 
     public Set<Pet> getPets() {
         return pets;
@@ -13,5 +16,27 @@ public class Owner extends Person {
         this.pets = pets;
     }
 
+    public String getAddress() {
+        return address;
+    }
 
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 }
