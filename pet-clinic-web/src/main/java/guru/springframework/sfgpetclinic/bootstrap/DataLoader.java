1895389c7de2da99cdea5009a23c567789ca4c9f
AndyWeasley2004 <m5yao@ucsd.edu>
2023-07-03 13:01:13 -0700

    Add Pets and Contact Info on startup. Closes #33

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -33,18 +36,25 @@ public class DataLoader implements CommandLineRunner {
         cat.setName("cat");
         PetType savedCatPetType = petTypeService.save(cat);
 
+        System.out.println("Loading owners...");
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Arthur");
         owner1.setLastName("Wong");
+        owner1.setAddress("9450 Gilman Drive");
+        owner1.setCity("La Jolla");
+        owner1.setTelephone("13866180134");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Aviva");
         owner2.setLastName("Xu");
+        owner1.setAddress("9450 Gilman Drive");
+        owner1.setCity("La Jolla");
+        owner1.setTelephone("8583051264");
         ownerService.save(owner2);
 
-        System.out.println("Loading owners...");
+        System.out.println("Loading Vets...");
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Angelina");
@@ -56,7 +66,21 @@ public class DataLoader implements CommandLineRunner {
         vet2.setLastName("Li");
         vetService.save(vet2);
 
-        System.out.println("Loading Vets...");
+        System.out.println("Loading Pets");
+
+        Pet dogOne = new Pet();
+        dogOne.setPetType(savedDogPetType);
+        dogOne.setOwner(owner1);
+        dogOne.setName("Tom");
+        dogOne.setBirthDate(LocalDate.now());
+        owner1.getPets().add(dogOne);
+
+        Pet catOne = new Pet();
+        catOne.setPetType(savedCatPetType);
+        catOne.setOwner(owner2);
+        catOne.setName("Zero");
+        catOne.setBirthDate(LocalDate.now());
+        owner2.getPets().add(catOne);
 
     }
 }
