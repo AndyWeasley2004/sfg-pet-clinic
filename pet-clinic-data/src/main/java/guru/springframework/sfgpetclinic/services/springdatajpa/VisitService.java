3dcebebbd87ec826796cac9ea760d44a17033f73
AndyWeasley2004 <m5yao@ucsd.edu>
2023-07-05 18:00:37 -0700

    Adding Map Service for Visit closes #47

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/VisitService.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.services.CrudService;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
