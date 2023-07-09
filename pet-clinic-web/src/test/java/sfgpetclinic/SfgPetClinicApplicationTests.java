1ede8dc2be577a617783b1fcb5937d68b01e1a18
AndyWeasley2004 <m5yao@ucsd.edu>
2023-07-09 14:48:09 -0700

    Upgrade to junit5 Closes #4

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,10 +1,12 @@
 package sfgpetclinic;
 
-import guru.springframework.sfgpetclinic.SfgPetClinicApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest(classes = SfgPetClinicApplication.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class SfgPetClinicApplicationTests {
 
     @Test
